15141e4b9b55dca4db54637b4c629256e4f22294
Lokuyow <cg.magnet@gmail.com>
2023-04-05 19:21:07 +0900

    move

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 function showMessage() {
-    alert("Hello World!");
+    alert("で？");
 }
+
+let position = 0;
+
+function moveButton() {
+    const button = document.getElementById("myButton");
+    position += 10;
+    button.style.marginLeft = position + "px";
+}
+
+setInterval(moveButton, 100);
